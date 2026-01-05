1b6606c0e0a875be3d1d5dc009db36d0863febd6
Shagun Mishra <shagunmmishra@gmail.com>
2026-01-05 15:35:42 +0530

    Expand skills in extract_skills function
    Added new skills to existing roles in the extract_skills function.

--- nlp/skill_matcher.py
+++ nlp/skill_matcher.py
@@ -1,11 +1,12 @@
 def extract_skills(text):
     skills = {
-        "web development": ["html", "css", "javascript", "react", "flask"],
-        "data analyst": ["sql", "excel", "power bi", "pandas"],
-        "machine learning": ["machine learning", "nlp", "tensorflow"],
-        "java developer": ["java", "spring", "hibernate"],
-        "cloud": ["aws", "docker"]
+        "web development": ["html", "css", "javascript", "react", "flask" , "django" , "node.js"],
+        "data analyst": ["sql", "excel", "power bi", "pandas" ,"numpy" , "matplotlib" , "tableau"],
+        "machine learning": ["machine learning", "nlp", "tensorflow","machine learning", "deep learning", "artificial intelligence","nlp", "computer vision","scikit-learn", "tensorflow", "keras", "pytorch","mlops", "model deployment",],
+        "java developer": ["java", "spring", "hibernate", "j2ee", "j2ee", "hibernate", "maven", "gradle", "microservices",  "oracle", "redis", "firebase"],
+        "cloud": ["aws", "docker", "aws", "azure", "gcp","ec2", "s3", "lambda", "docker", "kubernetes","ci/cd", "jenkins", "terraform", "cloudformation"]
     }
+
 
     text = text.lower()
     result = {}
